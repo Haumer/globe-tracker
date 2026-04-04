c21d17b30f4f60c2b23f09b3e111e126542ac6dc
Alexander Haumer <alexander.haumer@me.com>
2026-04-04 19:58:11 +0200

    Fix dev importmap invalidation after UI move

--- lib/app_revision.rb
+++ lib/app_revision.rb
@@ -2,12 +2,20 @@ module AppRevision
   module_function
 
   def current
-    @current ||= begin
-      ENV["APP_REVISION"].presence ||
-        ENV["SOURCE_VERSION"].presence ||
-        ENV["HEROKU_SLUG_COMMIT"].presence ||
-        ENV["HEROKU_RELEASE_VERSION"].presence ||
-        Rails.application.importmap.digest(resolver: ActionController::Base.helpers)
-    end
+    return resolved_revision unless cacheable_revision?
+
+    @current ||= resolved_revision
+  end
+
+  def cacheable_revision?
+    Rails.env.production?
+  end
+
+  def resolved_revision
+    ENV["APP_REVISION"].presence ||
+      ENV["SOURCE_VERSION"].presence ||
+      ENV["HEROKU_SLUG_COMMIT"].presence ||
+      ENV["HEROKU_RELEASE_VERSION"].presence ||
+      Rails.application.importmap.digest(resolver: ActionController::Base.helpers)
   end
 end

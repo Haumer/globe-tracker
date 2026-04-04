51f1a1f199b8648ba110171cb84fa2fc67ae2924
Alexander Haumer <alexander.haumer@me.com>
2026-04-04 14:29:49 +0200

    Fix merged sidebar integration

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -28,22 +28,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_match(/href="\/favicon-16x16\.png"/, response.body)
   end
 
-  test "home page includes social metadata and favicon links" do
-    get "/"
-
-    assert_response :success
-    assert_equal "no-store", response.headers["Cache-Control"]
-    assert_equal "no-cache", response.headers["Pragma"]
-    assert_equal "0", response.headers["Expires"]
-    assert_match(/name="app-revision" content="[a-f0-9]+"/, response.body)
-    assert_match(/property="og:title" content="GlobeTracker \| Live Global Tracking"/, response.body)
-    assert_match(/property="og:image" content="http:\/\/www\.example\.com\/og-card\.png"/, response.body)
-    assert_match(/name="twitter:card" content="summary_large_image"/, response.body)
-    assert_match(/rel="icon" href="\/favicon\.ico" sizes="any"/, response.body)
-    assert_match(/href="\/favicon-32x32\.png"/, response.body)
-    assert_match(/href="\/favicon-16x16\.png"/, response.body)
-  end
-
   test "sources page lists active source inventory entries without removed placeholders" do
     get "/sources"
 
